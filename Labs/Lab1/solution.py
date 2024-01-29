5a5c8089baf4293d2e27c813ae101e32bac7f255
PatoRoa <100495806+PatoRoa@users.noreply.github.com>
2024-01-29 13:06:01 -0800

    Update solution.py

--- Labs/Lab1/solution.py
+++ Labs/Lab1/solution.py
@@ -1,1 +1,6 @@
+nums = []
+target = int
 
+# Return the indices of the two numbers such that they add up to target
+# Create hashmap with key starting at zero and ascending and value starting with target and descending
+# If key + value = target, then yay
